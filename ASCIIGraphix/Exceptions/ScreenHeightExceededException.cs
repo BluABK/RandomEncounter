2a3008a8fde700517ed55be906d12d5ea7e12963
BluABK <bluabk@bluabk.net>
2021-09-15 23:17:14 +0200

    Created custom exceptions for when Screen width or height is exceeded.

---
+++ ASCIIGraphix/Exceptions/ScreenHeightExceededException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ASCIIGraphix.Exceptions
+{
+    [Serializable]
+    public class ScreenHeightExceededException : Exception
+    {
+        public ScreenHeightExceededException()
+        {
+        }
+
+        public ScreenHeightExceededException(string message)
+            : base(message)
+        {
+        }
+
+        public ScreenHeightExceededException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        // Ensure Exception is Serializable
+        protected ScreenHeightExceededException(SerializationInfo info, StreamingContext ctxt)
+            : base(info, ctxt)
+        {
+        }
+    }
+}
---
+++ ASCIIGraphix/Exceptions/ScreenWidthExceededException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ASCIIGraphix.Exceptions
+{
+    [Serializable]
+    public class ScreenWidthExceededException : Exception
+    {
+        public ScreenWidthExceededException()
+        {
+        }
+
+        public ScreenWidthExceededException(string message)
+            : base(message)
+        {
+        }
+
+        public ScreenWidthExceededException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        // Ensure Exception is Serializable
+        protected ScreenWidthExceededException(SerializationInfo info, StreamingContext ctxt)
+            : base(info, ctxt)
+        {
+        }
+    }
+}
