c488350567671810621ef037ab40e8c201a3c96f
BluABK <bluabk@bluabk.net>
2021-10-12 18:36:51 +0200

    Moved common heir properties and methods up to GfxObject itself.

--- ASCIIGraphix/GfxObjects/GfxObject.cs
+++ ASCIIGraphix/GfxObjects/GfxObject.cs
@@ -1,6 +1,43 @@
-﻿namespace ASCIIGraphix.GfxObjects
+﻿using System;
+
+namespace ASCIIGraphix.GfxObjects
 {
     public class GfxObject
     {
+        protected readonly IScreen MyScreen;
+        protected ConsoleColor DefaultBgColor { get; set; }
+        protected ConsoleColor DefaultFgColor { get; set; }
+        protected ConsoleColor BgColor { get; set; }
+        protected ConsoleColor FgColor { get; set; }
+
+        public GfxObject(IScreen screen)
+        {
+            MyScreen = screen;
+            DefaultBgColor = screen.DefaultBgColor;
+            DefaultFgColor = screen.DefaultFgColor;
+            BgColor = screen.BgColor;
+            FgColor = screen.FgColor;
+        }
+
+        /// <summary>
+        /// Turns a char c into string of n amount of c.
+        ///
+        /// Example:
+        ///     CharToString('a', 3) == "aaa"
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        protected static string CharToString(char c, int amount)
+        {
+            string s = string.Empty;
+
+            for (int i = 0; i < amount; ++i)
+            {
+                s += c;
+            }
+
+            return s;
+        }
     }
 }
